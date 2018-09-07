920a6b51dcbf775c72964a762f9c060fdc52e350
Tiffany Jachja <tjachja@redhat.com>
2018-09-07 09:38:29 -0400

    script fixes

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -35,23 +35,29 @@ echo "Granting permissions"
 oc policy add-role-to-user edit system:serviceaccount:$GUID-jenkins:jenkins -n $GUID-jenkins
 oc policy add-role-to-user edit system:serviceaccount:gpte-jenkins:jenkins -n $GUID-jenkins
 
-${ocp} new-app ${TEMPLATES_ROOT}/jenkins.yml
-${ocp} rollout status dc/$(${ocp} get dc -o jsonpath='{ .items[0].metadata.name }') -w 
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi
 
-cat ${TEMPLATES_ROOT}/slavepod.Dockerfile | ${ocp} new-build --name=jenkins-slave-appdev -D - 
+LIN_NUM=$(($(sed -n '/\[registries.insecure\]/=' /etc/containers/registries.conf) + 1))
+sed -i "${LIN_NUM}d" /etc/containers/registries.conf
+sed "${LIN_NUM}i registries = \['docker-registry-default.apps.na39.openshift.opentlc.com'\]" /etc/containers/registries.conf
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+mkdir -p $HOME/jenkins-slave-appdev
+cd  $HOME/jenkins-slave-appdev
+
+echo "FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9
+USER root
+RUN yum -y install skopeo apb && \
+    yum clean all
+USER 1001" > Dockerfile
+
+sudo docker build . -t docker-registry-default.apps.na39.example.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9
 
 
-${ocp} create -f ${TEMPLATES_ROOT}/mlbparks-pipeline.yaml -n ${GUID}-jenkins
-oc cancel-build bc/mlbparks-pipeline -n ${GUID}-jenkins || echo "build not cancelled"
-oc set env bc/mlbparks-pipeline GUID="$GUID" CLUSTER="$CLUSTER" -n ${GUID}-jenkins
-oc start-build bc/mlbparks-pipeline -n ${GUID}-jenkins
+skopeo copy --dest-tls-verify=false --dest-creds=$(oc whoami):$(oc whoami -t) docker-daemon:docker-registry-default.apps.${CLUSTER}/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9 docker://docker-registry-default.apps.${CLUSTER}/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.9	
 
-${ocp} create -f ${TEMPLATES_ROOT}/nationalparks-pipeline.yaml
-oc cancel-build bc/nationalparks-pipeline -n ${GUID}-jenkins || echo "build not cancelled"
-oc set env bc/nationalparks-pipeline GUID="$GUID" CLUSTER="$CLUSTER" -n ${GUID}-jenkins
-oc start-build bc/nationalparks-pipeline -n ${GUID}-jenkins
 
-${ocp} create -f ${TEMPLATES_ROOT}/parksmap-pipeline.yaml
-oc cancel-build bc/parksmap-pipeline -n ${GUID}-jenkins || echo "build not cancelled"
-oc set env buildconfigs/parksmap-pipeline GUID="$GUID" CLUSTER="$CLUSTER" -n ${GUID}-jenkins
-oc start-build bc/parksmap-pipeline -n ${GUID}-jenkins
+
+${ocp} create -f ${TEMPLATES_ROOT}/pipeline.yaml
